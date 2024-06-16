4342428934b92b66cac24b6dde31bb5b3acc4273
Ruth <ruro122020@gmail.com>
2024-06-16 19:08:51 -0400

    petassessment results page in progress

--- src/pages/petassessment/PetAssessmentResults.jsx
+++ src/pages/petassessment/PetAssessmentResults.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react'
 import { getData, postData } from '../../api'
 import { usePetAssessment } from '../../context/PetAssessmentContext'
 import { Box, Button, Typography } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 const PetAssessmentResults = () => {
   const { petInfo } = usePetAssessment()
   const [results, setResults] = useState([])
-
+  const navigate = useNavigate()
   useEffect(() => {
     const getResults = async () => {
       // petInfo.symptoms is an array of symptoms object and the api expects symptoms to be an array of 
@@ -21,12 +22,18 @@ const PetAssessmentResults = () => {
   }, [])
   console.log('results', results)
   return (
-    <Box>
-      <Typography variant="h5">Results</Typography>
-      {results.map(({ name }) => <p>{name}</p>)}
-      <Button>Save</Button>
-      <Button>Start Over</Button>
-
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Typography variant="h5" sx={{ fontWeight: 100 }}>Results</Typography>
+      {results.map(({ name, description }) => {
+        return <div style={{ textAlign: 'center' }}>
+          <p>{name.toUpperCase()}</p>
+          <p>{description}</p>
+        </div>
+      })}
+      <div style={{ display: 'flex' }}>
+        <Button onClick={() => navigate('/pet-assessment')}>Start Over</Button>
+        <Button onClick={() => navigate('/user/dashboard')}>Save</Button>
+      </div>
     </Box>
   )
 }
