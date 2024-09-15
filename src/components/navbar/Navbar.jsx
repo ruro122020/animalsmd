3aa6a7b66f007a61c3216f568ffbb60e6c623248
ruth <ruro122020@gmail.com>
2024-09-15 15:31:48 -0400

    install tailwindcss and flowbite

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import { NavLink } from 'react-router-dom'
-import { useAuth } from '../../context/AuthContext';
-import { deleteData, getData } from '../../api'
-import CartIcon from '../CartIcon'
-import { useCartContext } from '../../context/CartContext';
-import { pages, identity, settings } from './links'
+// import React, { useState, useEffect } from 'react'
+// import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+// import IconButton from '@mui/material/IconButton';
+// import Typography from '@mui/material/Typography';
+// import Menu from '@mui/material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import Container from '@mui/material/Container';
+// import Avatar from '@mui/material/Avatar';
+// import Button from '@mui/material/Button';
+// import Tooltip from '@mui/material/Tooltip';
+// import MenuItem from '@mui/material/MenuItem';
+// import { NavLink } from 'react-router-dom'
+// import { useAuth } from '../../context/AuthContext';
+// import { deleteData, getData } from '../../api'
+// import CartIcon from '../CartIcon'
+// import { useCartContext } from '../../context/CartContext';
+// import { pages, identity, settings } from './links'
 
+
+
+import React, { useState } from 'react'
+import { Button, Navbar as Nav } from 'flowbite-react'
+import Link from 'next/link';
 
 const Navbar = () => {
   //useAuth is from AuthContext.jsx file
   const { isLoggedIn, logout, updateUser, user } = useAuth()
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
   const { cartItemsCount, setCartItemsCount } = useCartContext()
 
   useEffect(() => {
@@ -39,222 +43,267 @@ const Navbar = () => {
   }, [isLoggedIn])
 
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  const handleLogout = async () => {
-    const success = await deleteData('/api/logout')
-    if (success) {
-      logout()
-      updateUser(null)
-    }
-  }
 
   return (
-    <AppBar position="sticky" sx={{ background: '#3995ae' }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            AnimalsMD
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map(({ route, page }) => (
-                <MenuItem
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  component={NavLink}
-                  to={route}
-                  sx={{ my: 2, display: 'block', textDecoration: 'none' }}
-                >
-                  {page}
-                </MenuItem>
-              ))}
-              {!isLoggedIn &&
-                identity.map(({ route, page }) => (
-                  <MenuItem
-                    key={page}
-                    onClick={handleCloseNavMenu}
-                    component={NavLink}
-                    to={route}
-                    sx={{ my: 2, display: 'block', textDecoration: 'none' }}
-                  >
-                    {page}
-                  </MenuItem>
-                ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-            }}
-          >
-            AnimalsMD
-          </Typography>
-          {/*This section is the desktop size */}
-          <Box sx={{
-            flexGrow: 1,
-            display: { xs: 'none', md: 'flex' },
-          }}>
-            <Box sx={{
-              flexGrow: 1,
-              display: { xs: 'none', md: 'flex' },
-            }}>
-              {pages.map(({ route, page }) => (
-                <Button
-                  key={page}
-                  as={NavLink}
-                  to={route}
-                  onClick={handleCloseNavMenu}
-                  sx={{
-                    my: 2, color: 'white', display: 'block', textAlign: 'end', textDecoration: 'none',
-                  }}
-                >
-                  {page}
-                </Button>
-              ))}
-            </Box>
-            <Box sx={{
-              flexGrow: 1,
-              display: { xs: 'none', md: 'flex' },
-              justifyContent: 'end'
-            }}>
-              {!isLoggedIn &&
-                identity.map(({ route, page }) => (
-                  <Button
-                    key={page}
-                    as={NavLink}
-                    to={route}
-                    onClick={handleCloseNavMenu}
-                    sx={{ my: 2, color: 'white', display: 'block', textDecoration: 'none' }}
-                  >
-                    {page}
-                  </Button>
-                ))}
-            </Box>
-          </Box>
-          {isLoggedIn && cartItemsCount > 0 && <CartIcon />}
-          {
-            isLoggedIn &&
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar
-                    alt="Your Photo"
-                  // src={user.image_url} 
-                  />
-
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map(({ route, page }) => (
-                  <MenuItem key={page} onClick={handleCloseUserMenu}>
-                    <Typography
-                      textAlign="center"
-                      as={NavLink}
-                      to={route}
-                      sx={{
-                        textDecoration: 'none',
-                      }}
-                    >{page}</Typography>
-                  </MenuItem>
-                ))}
-                <MenuItem onClick={handleCloseUserMenu}>
-                  <Typography
-                    textAlign="center"
-                    as={NavLink}
-                    to='/'
-                    onClick={handleLogout}
-                    sx={{ textDecoration: 'none' }}
-                  >Logout</Typography>
-                </MenuItem>
-              </Menu>
-            </Box>
+    <Nav fluid rounded className='bg-dark-grey-1'>
+      <Nav.Brand href="https://flowbite-react.com">
+        {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="animalsmd" /> */}
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">AnimalsMD</span>
+      </Nav.Brand>
+      <div className="flex md:order-2">
+        {/**Get started on Pet assessment */}
+        <Button className=''>Get started</Button>
+        <Nav.Toggle />
+      </div>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
           }
-        </Toolbar>
-      </Container>
-    </AppBar>
-
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+      </div>
+      <Nav.Collapse>
+        <Nav.Link as={Link} href="/" className='text-light-grey' >Home</Nav.Link>
+        <Nav.Link as={Link} href="/products" className='text-light-grey'>Products</Nav.Link>
+        <Nav.Link as={Link} href="/signup" className='text-light-grey'>Signup</Nav.Link>
+        <Nav.Link as={Link} href="/login" className='text-light-grey'>Login</Nav.Link>
+      </Nav.Collapse>
+    </Nav>
   )
 }
 
 export default Navbar
+
+
+
+{/**OLD VERSION OF NAVBAR */ }
+
+//   const [anchorElNav, setAnchorElNav] = React.useState(null);
+//   const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+//   const handleOpenNavMenu = (event) => {
+//     setAnchorElNav(event.currentTarget);
+//   };
+//   const handleOpenUserMenu = (event) => {
+//     setAnchorElUser(event.currentTarget);
+//   };
+
+//   const handleCloseNavMenu = () => {
+//     setAnchorElNav(null);
+//   };
+
+//   const handleCloseUserMenu = () => {
+//     setAnchorElUser(null);
+//   };
+
+//   const handleLogout = async () => {
+//     const success = await deleteData('/api/logout')
+//     if (success) {
+//       logout()
+//       updateUser(null)
+//     }
+//   }
+
+//   <AppBar position="sticky" sx={{ background: '#3995ae' }}>
+// <Container maxWidth="xl">
+//   <Toolbar disableGutters>
+//     <Typography
+//       variant="h6"
+//       noWrap
+//       component="a"
+//       href="#app-bar-with-responsive-menu"
+//       sx={{
+//         mr: 2,
+//         display: { xs: 'none', md: 'flex' },
+//         fontFamily: 'monospace',
+//         fontWeight: 700,
+//         letterSpacing: '.3rem',
+//         color: 'inherit',
+//         textDecoration: 'none',
+//       }}
+//     >
+//       AnimalsMD
+//     </Typography>
+
+//     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+//       <IconButton
+//         size="large"
+//         aria-label="account of current user"
+//         aria-controls="menu-appbar"
+//         aria-haspopup="true"
+//         onClick={handleOpenNavMenu}
+//         color="inherit"
+//       >
+//         <MenuIcon />
+//       </IconButton>
+//       <Menu
+//         id="menu-appbar"
+//         anchorEl={anchorElNav}
+//         anchorOrigin={{
+//           vertical: 'bottom',
+//           horizontal: 'left',
+//         }}
+//         keepMounted
+//         transformOrigin={{
+//           vertical: 'top',
+//           horizontal: 'left',
+//         }}
+//         open={Boolean(anchorElNav)}
+//         onClose={handleCloseNavMenu}
+//         sx={{
+//           display: { xs: 'block', md: 'none' },
+//         }}
+//       >
+//         {pages.map(({ route, page }) => (
+//           <MenuItem
+//             key={page}
+//             onClick={handleCloseNavMenu}
+//             component={NavLink}
+//             to={route}
+//             sx={{ my: 2, display: 'block', textDecoration: 'none' }}
+//           >
+//             {page}
+//           </MenuItem>
+//         ))}
+//         {!isLoggedIn &&
+//           identity.map(({ route, page }) => (
+//             <MenuItem
+//               key={page}
+//               onClick={handleCloseNavMenu}
+//               component={NavLink}
+//               to={route}
+//               sx={{ my: 2, display: 'block', textDecoration: 'none' }}
+//             >
+//               {page}
+//             </MenuItem>
+//           ))}
+//       </Menu>
+//     </Box>
+//     <Typography
+//       variant="h5"
+//       noWrap
+//       component="a"
+//       href="#app-bar-with-responsive-menu"
+//       sx={{
+//         mr: 2,
+//         display: { xs: 'flex', md: 'none' },
+//         flexGrow: 1,
+//         fontFamily: 'monospace',
+//         fontWeight: 700,
+//         letterSpacing: '.3rem',
+//         color: 'inherit',
+//       }}
+//     >
+//       AnimalsMD
+//     </Typography>
+//     //This section is the desktop size 
+// <Box sx={{
+//   flexGrow: 1,
+//   display: { xs: 'none', md: 'flex' },
+// }}>
+//   <Box sx={{
+//     flexGrow: 1,
+//     display: { xs: 'none', md: 'flex' },
+//   }}>
+//     {pages.map(({ route, page }) => (
+//       <Button
+//         key={page}
+//         as={NavLink}
+//         to={route}
+//         onClick={handleCloseNavMenu}
+//         sx={{
+//           my: 2, color: 'white', display: 'block', textAlign: 'end', textDecoration: 'none',
+//         }}
+//       >
+//         {page}
+//       </Button>
+//     ))}
+//   </Box>
+//   <Box sx={{
+//     flexGrow: 1,
+//     display: { xs: 'none', md: 'flex' },
+//     justifyContent: 'end'
+//   }}>
+//     {!isLoggedIn &&
+//       identity.map(({ route, page }) => (
+//         <Button
+//           key={page}
+//           as={NavLink}
+//           to={route}
+//           onClick={handleCloseNavMenu}
+//           sx={{ my: 2, color: 'white', display: 'block', textDecoration: 'none' }}
+//         >
+//           {page}
+//         </Button>
+//       ))}
+//   </Box>
+// </Box>
+// { isLoggedIn && cartItemsCount > 0 && <CartIcon /> }
+// {
+//   isLoggedIn &&
+//     <Box sx={{ flexGrow: 0 }}>
+//       <Tooltip title="Open settings">
+//         <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+//           <Avatar
+//             alt="Your Photo"
+//           // src={user.image_url} 
+//           />
+
+//         </IconButton>
+//       </Tooltip>
+//       <Menu
+//         sx={{ mt: '45px' }}
+//         id="menu-appbar"
+//         anchorEl={anchorElUser}
+//         anchorOrigin={{
+//           vertical: 'top',
+//           horizontal: 'right',
+//         }}
+//         keepMounted
+//         transformOrigin={{
+//           vertical: 'top',
+//           horizontal: 'right',
+//         }}
+//         open={Boolean(anchorElUser)}
+//         onClose={handleCloseUserMenu}
+//       >
+//         {settings.map(({ route, page }) => (
+//           <MenuItem key={page} onClick={handleCloseUserMenu}>
+//             <Typography
+//               textAlign="center"
+//               as={NavLink}
+//               to={route}
+//               sx={{
+//                 textDecoration: 'none',
+//               }}
+//             >{page}</Typography>
+//           </MenuItem>
+//         ))}
+//         <MenuItem onClick={handleCloseUserMenu}>
+//           <Typography
+//             textAlign="center"
+//             as={NavLink}
+//             to='/'
+//             onClick={handleLogout}
+//             sx={{ textDecoration: 'none' }}
+//           >Logout</Typography>
+//         </MenuItem>
+//       </Menu>
+//     </Box>
+// }
+//   </Toolbar >
+// </Container >
+// </AppBar > 
