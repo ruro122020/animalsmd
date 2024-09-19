77e452182a29b5c53da56afa48da3f59d236a5d1
ruth <ruro122020@gmail.com>
2024-09-19 12:52:33 -0400

    deployment in progress

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+
+
 //IMPORTANT NOTE: VITE CONNECTION MUST BE CONFIGURED FOR COOKIES TO WORK!
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_BASE_URL_PRODUCTION,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          // To access env vars here use process.env.TEST_VAR
+          target: process.env.VITE_API_BASE_URL_PRODUCTION,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-})
+  })
+}
+
+
+
+
 
